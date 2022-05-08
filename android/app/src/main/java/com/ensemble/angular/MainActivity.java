0c7ab2b1149ce67a3ce02bb4ef4ecafc0775b538
ShaunM <32672907+Shaun-Mifsud@users.noreply.github.com>
2022-05-08 16:06:01 +0200

    Android build

---
+++ android/app/src/main/java/com/ensemble/angular/MainActivity.java
@@ -0,0 +1,5 @@
+package com.ensemble.angular;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ensemble.angular',
+  appName: 'Ensemble',
+  webDir: 'dist/Ensemble',
+  bundledWebRuntime: false
+};
+
+export default config;
